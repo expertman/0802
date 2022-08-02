68add20f8461a9f44ec6a5e277fd0ac3f347741a
expertman <javaexpert@nate.com>
2022-08-02 17:52:48 +0900

    Create 8월 2일 프로젝트

---
+++ src/MethodDemo3.java
@@ -0,0 +1,19 @@
+
+public class MethodDemo3 {
+	public static void main(String[] args) {
+		//System.out.println(mymethod(50));
+	}
+	static Student mymethod() {
+		Student jimin = new Student();
+		jimin.name = "한지민";
+		jimin.age = 26;
+		return jimin;
+	}
+//	static int [] mymethod() {
+//		return new int[]{5, 9,4,5,6,7,8,9};
+//	}
+//	static String mymethod(int su) {  //한번, 한개만 리턴
+//		if(su % 2 == 0) return "Hello";
+//		else return "World";
+//	}
+}
